6599ed2719bf83aa7a5ad7b034da324cee5e001c
Hestri074 <20523074@students.uii.ac.id>
2025-10-19 23:35:11 +0700

    Auth & Security

--- social/social/src/main/java/id/go/bpddiy/social/SocialApplication.java
+++ social/social/src/main/java/id/go/bpddiy/social/SocialApplication.java
@@ -2,12 +2,8 @@ package id.go.bpddiy.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-// @EntityScan(basePackages = { "id.go.bpddiy.social.model" })
-// @EnableJpaRepositories
 public class SocialApplication {
 
 	public static void main(String[] args) {
